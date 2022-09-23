8993ab09512c3c06860de50b516745724941a96f
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2022-09-23 16:39:01 -0400

    project wip

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Fragment } from "react";
 import Default from "../components/layout/default";
+import {get} from "../lib/api"
 
 const projectsData = [
   {
@@ -47,34 +48,52 @@ const otherProjectsData = [
   },
 ];
 
-function Homepage() {
+
+export async function getServerSideProps(context) {
+  const { data, message , isError } = await get('http://localhost:3000/project');
+  return {
+    props: {
+      projects : data,
+      message,
+      isError
+    },
+  }
+}
+
+function Homepage({projects, message, isError}) {
+  const mainProjects = projects.filter(project => project.main_project === true)
+  const otherProjects = projects.filter(project => project.main_project === false)
   return (
     <Default>
       <div className="h-full flex flex-col items-end justify-end">
         <div className="flex flex-col items-end justify-end w-fit max-w-[600px]">
-          {projectsData.map((project) => (
-            <Link key={project.id} href={"#"} passHref>
-              <a
-                href="#"
-                alt="project"
-                className="whitespace-nowrap uppercase text-[56px] text-grey cursor-pointer hover:text-white"
-              >
-                {project.label}
-              </a>
+          {isError && <div className="text-red">{message}</div>}
+          {mainProjects.map((project) => (
+            <Link key={project._id} href={"#"} passHref>
+              <div className="flex flex-col items-end hover:text-red">
+                <a
+                  href="#"
+                  alt="project"
+                  className="whitespace-nowrap uppercase text-[56px] text-grey cursor-pointer hover:text-white"
+                >
+                  {project.project_name}
+                </a>
+                <p className="text-[16px] normal-case text-grey">{project.project_stack}</p>
+              </div>
             </Link>
           ))}
           <div className="flex flex-wrap items-end justify-end overflow-hidden">
             <span className="text-[16px] normal-case text-grey cursor-pointer">
               Other projects:
             </span>
-            {otherProjectsData.map((project) => (
+            {otherProjects.map((project) => (
               <Link key={project.id} href={"#"} passHref>
                 <a
                   href={"#"}
                   alt="project"
                   className="relative text-[16px] normal-case text-grey cursor-pointer ml-[25px] after:w-[5px] after:h-[5px] after:rounded after:bg-grey after:absolute after:right-[-15px] after:top-[10px] hover:text-white"
                 >
-                  {project.label}
+                  {project.project_name}
                 </a>
               </Link>
             ))}
@@ -84,4 +103,5 @@ function Homepage() {
     </Default>
   );
 }
+
 export default Homepage;
