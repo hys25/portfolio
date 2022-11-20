196aa7e36a2089eb9a86339f80119f29d14980c4
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2022-11-20 14:19:42 -0500

    hover on links

--- components/elements/downloadCv.js
+++ components/elements/downloadCv.js
@@ -23,8 +23,13 @@ function DownloadCv({ className }) {
       alt="YavtushenkoHalynaFrontendEngineer.pdf"
       className={`flex flex-wrap justify-center items-center ml-6 w-6 mb-[110px] ${className}`}
     >
-      <div className="transition-colors relative text-[12px] mb-1 uppercase text-grey hover:text-white before:inline-block before:absolute before:w-5 before:h-5 before:hover:transition-all before:bg-[url('/icons/download-cv-grey.svg')] before:right-[-1px] before:bottom-[-28px] before:bg-no-repeat before:bg-contain hover:before:bg-[url('/icons/download-cv.png')]">
-        Download cv
+      <div className="flex transition-colors relative text-[12px] mb-1 uppercase text-grey hover:text-white before:inline-block before:absolute before:w-5 before:h-5 before:hover:transition-all before:bg-[url('/icons/download-cv-grey.svg')] before:right-[-1px] before:bottom-0 before:bg-no-repeat before:bg-contain hover:before:bg-[url('/icons/download-cv.png')]">
+        <span className="mb-[20px]">Download cv</span>
+        <img
+          alt="Arrow down"
+          className="object-contain bg-no-repeat bg-contain h-[20px] w-[20px]"
+          src="./../../icons/download-cv-grey.svg"
+        />
       </div>
     </StyledLink>
   )
--- components/elements/socialLinks.js
+++ components/elements/socialLinks.js
@@ -1,6 +1,6 @@
 import Link from "next/link"
 
-function SocialLinks({ className, title = null }) {
+function SocialLinks({ className = "", title = "" }) {
   return (
     <div
       className={`flex flex-col items-center pl-6 mb-5 w-[50px] ${className}`}
@@ -10,13 +10,18 @@ function SocialLinks({ className, title = null }) {
           href="https://github.com/hys25"
           alt="github"
           target="_blank"
-          className={`relative h-6 text-grey before:inline-block before:absolute before:w-5 before:h-5 before:hover:transition-all before:bg-[url('/icons/link-github-grey.svg')] before:bg-no-repeat before:bg-contain hover:before:bg-[url('/icons/link-github.png')] mb-6 transition-all  ${
+          className={`relative h-6 w-6 text-grey before:block before:absolute before:w-6 before:h-6 before:bg-[url('/icons/link-github-grey.svg')] before:transition-all before:bg-no-repeat before:bg-contain hover:before:bg-[url('/icons/link-github.png')] mb-6 transition-all ${
             title
-              ? "before:bottom-[5px] uppercase before:left-0 w-full pl-8"
-              : "before:bottom-0 before:right-1 w-6 indent-[100%] whitespace-nowrap overflow-hidden"
+              ? "before:bottom-[6px] uppercase before:left-0 w-full pl-8"
+              : "before:bottom-0 before:right-[1px] w-6 indent-[100%] whitespace-nowrap overflow-hidden"
           }`}
           rel="noreferrer"
         >
+          <img
+            alt="Arrow down"
+            className="object-contain bg-no-repeat bg-contain w-[24px]"
+            src="./../../icons/link-github-grey.png"
+          />
           Github
         </a>
       </Link>
@@ -25,13 +30,18 @@ function SocialLinks({ className, title = null }) {
           href="https://www.linkedin.com/in/halyna-yavtushenko/"
           alt="linkedin"
           target="_blank"
-          className={`relative h-6 text-grey before:inline-block before:absolute before:w-5 before:h-5 before:hover:transition-all before:bg-[url('/icons/link-linkedin-grey.svg')] before:bg-no-repeat before:bg-contain hover:before:bg-[url('/icons/link-linkedin.png')] mb-2 transition-all  ${
+          className={`relative h-6 w-6 text-grey before:block before:absolute before:w-6 before:h-6 before:transition-all before:bg-[url('/icons/link-linkedin-grey.svg')] before:bg-no-repeat before:bg-contain hover:before:bg-[url('/icons/link-linkedin.png')] mb-2 transition-all ${
             title
               ? "before:bottom-[6px] uppercase before:left-0 w-full pl-8"
-              : "before:bottom-0 before:right-1 w-6 indent-[100%] whitespace-nowrap overflow-hidden"
+              : "before:bottom-0 before:right-[1px] w-6 indent-[100%] whitespace-nowrap overflow-hidden"
           }`}
           rel="noreferrer"
         >
+          <img
+            alt="Arrow down"
+            className="object-contain bg-no-repeat bg-contain w-[24px]"
+            src="./../../icons/link-linkedin-grey.png"
+          />
           Linkedin
         </a>
       </Link>
